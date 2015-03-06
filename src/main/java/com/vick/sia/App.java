2c4a5db496214c4badf05dadcba264fb8ec1638d
vick <gas3111@gmail.com>
2015-03-05 17:02:05 -0800

    first commit

---
+++ src/main/java/com/vick/sia/App.java
@@ -0,0 +1,13 @@
+package com.vick.sia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
